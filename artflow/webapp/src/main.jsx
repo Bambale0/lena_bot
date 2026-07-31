8ef0c72f00d329e05e4ba7290f499bbf61f2398b
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-01 02:35:44 +0300

    feat(front-v2): enable Velvet Luxe visual layer

--- artflow/webapp/src/main.jsx
+++ artflow/webapp/src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from "react-dom/client";
 import VelvetApp from "./v2/VelvetApp.jsx";
 import "./v2/velvet-neon.css";
 import "./v2/velvet-typography.css";
+import "./v2/velvet-luxe.css";
 
-window.__APIX_MINIAPP_BUILD_ID__ = "20260801-velvet-neon-typography-v1";
+window.__APIX_MINIAPP_BUILD_ID__ = "20260801-velvet-luxe-concept-v3";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
