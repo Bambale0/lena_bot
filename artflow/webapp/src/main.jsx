044d36cf6d7685f1459d823bcdd02620de377653
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-01 02:24:16 +0300

    style: wire premium typography into front v2

--- artflow/webapp/src/main.jsx
+++ artflow/webapp/src/main.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import VelvetApp from "./v2/VelvetApp.jsx";
 import "./v2/velvet-neon.css";
+import "./v2/velvet-typography.css";
 
-window.__APIX_MINIAPP_BUILD_ID__ = "20260801-velvet-neon-front-v2";
+window.__APIX_MINIAPP_BUILD_ID__ = "20260801-velvet-neon-typography-v1";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
