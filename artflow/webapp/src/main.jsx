1ca25e9097232e5d5b763cb44a9f0482044e0aaa
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-01 10:15:25 +0300

    feat(webapp): switch miniapp entrypoint to v4 clean shell

--- artflow/webapp/src/main.jsx
+++ artflow/webapp/src/main.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./apix/App.jsx";
-import "./apix/apix.tokens.css";
-import "./apix/apix.css";
-import "./apix/apix-art.css";
-import "./apix/apix.archive.css";
-import "./apix/apix.final-pass.css";
-import "./apix/apix.structural.css";
-import "./apix/apix.v3.css";
+import App from "./apix/AppV4.jsx";
+import "./apix/apix.v4.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
