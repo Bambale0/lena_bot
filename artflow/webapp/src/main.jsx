6540d0472da9ef9419db4ccccae01801cc71bbc4
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-01 07:05:43 +0300

    feat(webapp): load archive visual pass after base styles

--- artflow/webapp/src/main.jsx
+++ artflow/webapp/src/main.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./apix/App.jsx";
+import "./apix/apix.tokens.css";
 import "./apix/apix.css";
 import "./apix/apix-art.css";
+import "./apix/apix.archive.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
