6de044b24e40d6a3753c14989240f98ff906f512
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-01 08:11:04 +0300

    fix(webapp): load final APIX layout pass

--- artflow/webapp/src/main.jsx
+++ artflow/webapp/src/main.jsx
@@ -5,6 +5,8 @@ import "./apix/apix.tokens.css";
 import "./apix/apix.css";
 import "./apix/apix-art.css";
 import "./apix/apix.archive.css";
+import "./apix/apix.final-pass.css";
+import "./apix/apix.final-pass.js";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
