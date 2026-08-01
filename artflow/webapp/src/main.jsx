c7065311fb089c3b03ffc31a313ef36642d815a8
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-01 06:42:41 +0300

    fix(webapp): load premium art override layer

--- artflow/webapp/src/main.jsx
+++ artflow/webapp/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./apix/App.jsx";
 import "./apix/apix.css";
+import "./apix/apix-art.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
