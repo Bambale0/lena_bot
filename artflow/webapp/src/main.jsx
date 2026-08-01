3a344a7a4627155c429cc91405a910a05255e7d3
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-01 03:56:25 +0300

    feat(frontend): enable premium concept layer

--- artflow/webapp/src/main.jsx
+++ artflow/webapp/src/main.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import ConceptApp from "./concept/App.jsx";
 import "./concept/concept.css";
+import "./concept/concept-premium.css";
 
-window.__APIX_MINIAPP_BUILD_ID__ = "20260801-velvet-concept-v1";
+window.__APIX_MINIAPP_BUILD_ID__ = "20260801-velvet-concept-studio-v2";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
