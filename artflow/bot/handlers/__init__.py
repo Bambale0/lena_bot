f08a0dc16a31c547f66ef469dee49c7b03c27c25
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-26 04:37:06 +0300

    fix: install image reference prompt guard

--- artflow/bot/handlers/__init__.py
+++ artflow/bot/handlers/__init__.py
@@ -11,6 +11,7 @@ from api.repeat_runtime import install_image_launch_snapshot
 from bot.keyboards import models as _model_keyboards
 from bot.services.grok_versions import install_grok_versions
 from bot.services.image_family_routing import install_image_family_routing
+from bot.services.image_reference_prompt_flow import install_image_reference_prompt_flow
 from bot.services.minimax_h3_ui import install_minimax_h3_handler_presentation
 from bot.services.veo_ui import install_veo_handler_presentation
 from bot.services.video_reference_support import install_video_reference_support
@@ -61,6 +62,7 @@ from . import repeat_references as _repeat_references  # noqa: F401
 from . import repeat_safe as _repeat_safe
 
 install_image_launch_snapshot(_legacy_image_gen)
+install_image_reference_prompt_flow(_legacy_image_gen)
 _repeat_callback_guard.install_repeat_confirmation_guard(_repeat_safe)
 _photo_prompt.install_photo_prompt_keyboard_hooks(_legacy_image_gen)
 
@@ -92,5 +94,5 @@ _video_router.include_router(_minimax_h3_references.router)
 _video_router.include_router(_seedance25_references.router)
 _video_router.include_router(_video_references.router)
 _video_router.include_router(_video_wizard.router)
-_video_router.include_router(_legacy_video_gen.router)
+_image_router.include_router(_legacy_image_gen.router)
 _legacy_video_gen.router = _video_router
